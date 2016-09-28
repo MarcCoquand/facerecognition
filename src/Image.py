4eea301bfaa75cb4ffbf7cae6a7874498dcb63d6
syltman <mcoquand@gmail.com>
2016-09-28 14:28:09 +0200

    implemented Image constructor

--- src/Image.py
+++ src/Image.py
@@ -3,9 +3,18 @@ class Image:
     """ Image is a utility class that takes care of necessary
         interactions with images """
 
-    def __init__(self):
-        return
+    def __init__(self, img):
+        for i in len(img):
+            img_arr = img[i].split(' ')
+        self.img.append(img_arr)
     
+    def set_ans(self,ans):
+        self.ans = ans
+
+    def get_ans(self):
+        return self.ans
+
     def rotate(degrees):
+        # TODO: IMPLEMENT
         # Rotation matrix is m = [cos(theta) -sin(theta); sin(theta) cos(theta)]
         return 
