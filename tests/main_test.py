b4d2905398abb7c79d7db95c1ae66ef218fb4e18
syltman <mcoquand@gmail.com>
2016-09-28 14:52:20 +0200

    implemented test_parse_facit

--- tests/main_test.py
+++ tests/main_test.py
@@ -2,6 +2,7 @@ import unittest
 from src.Main import *
 
 TRAINING_FILE = "../data/training.txt"
+TRAINING_FILE_ANS = "../data/training-facit.txt"
 
 
 class MainTest(unittest.TestCase):
@@ -13,4 +14,7 @@ class MainTest(unittest.TestCase):
             self.assertTrue(isinstance(img_list[i], Image([]).__class__))
 
     def test_parse_facit(self):
-        return
+        ans_list = parse_facit(TRAINING_FILE_ANS)
+        self.assertNotEquals(len(img_list), 0)
+        self.assertEquals(ans_list[0],2)
+        self.assertEquals(ans_list[12],3)
