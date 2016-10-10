8f07a65e3f959cfd35bb7df204ebff20f2611b7f
syltman <mcoquand@gmail.com>
2016-10-10 14:06:44 +0200

    added haskell file

---
+++ haskell/main.hs
@@ -0,0 +1,102 @@
+import System.Random
+import Data.List
+import Data.Ord
+import System.Environment
+import Data.Char
+import Data.List.Split
+
+-- ------------------------------------------------------------------------------
+-- | TYPE DEFINITIONS 
+
+-- | Perceptron consist of id from 1-4 and it's weights
+type Perceptron = (Integer, [Integer])
+type Ans        = Integer
+type Pix        = [Integer]
+type Img        = [(Pix, Ans)]
+
+-- ------------------------------------------------------------------------------
+-- | PERCEPTRON
+-- | Perceptron represents a perceptron in the network. A perceptron
+-- | represent one of the four feelings specified in the TYPE field.
+-- | It reacts to an input image with a response based on it's training.
+
+-- | Process the image with a perceptron
+process :: Perceptron -> Img -> Integer
+process _ []      = 0
+process (_,[]) _	= 0
+process p i = sum (map sum (q p i))
+				where
+				q :: Perceptron -> Img -> [[Integer]]
+				q _ []		 = []
+				q (_,[]) _ = []
+				q p (i:is) = ((zipWith (*) (fst i) (snd p))):(q p is)
+
+
+-- | Generate weights of length l
+weightGen :: Int -> StdGen -> [Float]
+weightGen n = take n . unfoldr (Just . random)
+
+-- | Update the weights with a delta error
+weightUpd :: [Float] -> [Float] -> [Float]
+weightUpd fw fd = zipWith (+) fw fd
+
+-- | Activation given a sum of weighted input
+computeAct :: Float -> Float
+computeAct f = 1/(1 + exp (f))
+
+-- ------------------------------------------------------------------------------
+-- | EXAMINER
+
+examine :: (Img -> String) -> Img -> [String]
+examine f = map f Img
+
+-- ------------------------------------------------------------------------------
+-- | IMAGE
+-- | Image represents an image that holds a list of pixels, id, and what
+-- | facial expression the image represents.
+
+-- | Take out all integers in string and convert to [Integer]
+toIntL :: String -> [Integer]
+toIntL [] = []
+toIntL s  = map (read::String->Integer) (splitOn " " s)
+
+-- | Filter noise in image, i.e. all values less than 3.
+noiseF :: Img -> Img
+noiseF []     = []
+noiseF (i:is) = (map (\x -> if x<3 then 0 else x) (fst i), (snd i)):noiseF is
+
+-- | Parse the image txt file and convert it to a matrix of strings
+--   to later be converted with toIntL
+isName :: String -> Bool
+isName (ms:_) = not $ isDigit ms
+isName []     = True
+
+fAux :: [[String]] -> [String] -> [String] -> [[String]]
+fAux acc us [] = reverse (reverse us:acc)
+fAux acc us (s:ss) 
+  | isName s  = fAux (us:acc) [] ss
+  | otherwise = fAux acc (s:us) ss
+
+-- | Takes lines of answers, removes the first word and returns the last one
+--   that should be a digit
+pAns :: [String] -> [Ans]
+pAns [] = []
+pAns (s@(ms:_):as)  
+  | ms == '#' = (pAns as)
+  | otherwise = (read (last (splitOn " " s))):(pAns as)
+
+-- ------------------------------------------------------------------------------
+-- | MAIN
+
+main :: IO ()
+main = do 
+  [p,a] <- getArgs
+  pF <- readFile p
+  pA <- readFile a
+  seed <- newStdGen
+	-- | Read both files and parse them, create a list of img, I.e. a face matching
+	--   an answer
+  let i  = zip (map concat (filter (not . null)  
+								(map (map toIntL) (fAux [] [] (lines pF))))) (pAns (lines pA))
+      iF = noiseF i
+  putStrLn pF
